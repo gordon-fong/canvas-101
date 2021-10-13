aec5736a0c288db8a937a2573f506333b43abe88
Matthew B. Gray <hey@matthew.nz>
2021-10-13 20:18:29 +1300

    circle moves

--- canvas.js
+++ canvas.js
@@ -8,20 +8,48 @@ let c = canvas.getContext('2d')
 
 // c.fillRect(100, 100, 100, 100)
 
-for (let i  = 0; i < 10000; i++) {
-  let xPos = Math.random() * window.innerWidth
-  let yPos = Math.random() * window.innerHeight
-  let minRadius = 5
-  let maxRadius = 20
-  let randRadius = Math.random() * (maxRadius - minRadius) + minRadius
+// for (let i  = 0; i < 10000; i++) {
+//   let xPos = Math.random() * window.innerWidth
+//   let yPos = Math.random() * window.innerHeight
+//   let minRadius = 5
+//   let maxRadius = 20
+//   let randRadius = Math.random() * (maxRadius - minRadius) + minRadius
+
+//   c.beginPath()
+//   c.arc(xPos, yPos, randRadius, 0, Math.PI*2, false)
+//   c.strokeStyle = `${randColor()}`
+//   c.stroke()
+// } 
+
+let xPos = Math.random() * window.innerWidth
+let yPos = Math.random() * window.innerHeight
+let xDir = (Math.random() - 0.5) * 10
+let yDir = (Math.random() - 0.5) * 10
+let minRadius = 5
+let maxRadius = 20
+let randRadius = Math.random() * (maxRadius - minRadius) + minRadius
+
+function animate() {
+  requestAnimationFrame(animate)
+  c.clearRect(0, 0, innerWidth, innerHeight)
 
   c.beginPath()
-  c.arc(xPos, yPos, randRadius, 0, Math.PI*2, false)
+  c.arc(xPos, yPos, randRadius, 0, Math.PI * 2, false)
   c.strokeStyle = `${randColor()}`
   c.stroke()
-} 
 
-function randColor () {
+  if (xPos > innerWidth || xPos < 0) {
+    xDir = -xDir
+  }
+  if (yPos > innerHeight || yPos < 0) {
+    yDir = -yDir
+  }
+
+  xPos += xDir
+  yPos += yDir
+}
+
+function randColor() {
   return (
     `rgb(
       ${Math.floor(Math.random() * 256)},
@@ -30,3 +58,5 @@ function randColor () {
     )`
   )
 }
+
+animate()
